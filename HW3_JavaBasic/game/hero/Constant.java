4055a03193a73fad4fc84e7a0cacaf8e4e17b7b6
AlexanderIsai <74790416+AlexanderIsai@users.noreply.github.com>
2023-05-05 01:32:47 +0300

    Homework 3. Java Basic

---
+++ HW3_JavaBasic/game/hero/Constant.java
@@ -0,0 +1,9 @@
+package Homework.JavaBasicHW3.game.hero;
+
+public class Constant {
+
+    public static final int ELF_POWER = 30;
+    public static final int ELF_CHARISMA = 50;
+    public static final int ELF_MAGIC = 100;
+
+}
