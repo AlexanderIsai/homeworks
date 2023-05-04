3e3fa8cf0118bbea0f98b78f464b8eb149cc4550
AlexanderIsai <74790416+AlexanderIsai@users.noreply.github.com>
2023-05-05 01:34:19 +0300

    Homework 3. Java Basic

---
+++ HW3_JavaBasic/Homework3.java
@@ -0,0 +1,28 @@
+package Homework.JavaBasicHW3;
+
+public class Homework3 {
+    static String str = new String("I study Basic Java!");
+    public static void handlingString (String str){
+
+        System.out.println(str.charAt(str.length()-1));
+
+        if (str.contains("Java")){
+            System.out.println("Строка содержит слово 'Java' это - " + str.contains("Java"));
+        } else System.out.println("Строка не содержит слово 'Java' это - " + str.contains("Java"));
+
+        System.out.println(str.replace('a', 'o'));
+
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.substring(0, 14));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Начало работы программы");
+
+        handlingString(str);
+        System.out.println("Конец работы программы");
+
+
+    }
+}
